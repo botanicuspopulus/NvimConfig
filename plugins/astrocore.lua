0dcb9bd3d15b6760ba70e0b58661bbd3fe63bf8c
William Smith <william.smith@hensoldt.net>
2024-04-10 19:01:05 +0200

    New astronvim files

---
+++ plugins/astrocore.lua
@@ -0,0 +1,103 @@
+-- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
+-- Configuration documentation can be found with `:h astrocore`
+-- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
+--       as this provides autocomplete and documentation while editing
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    -- Configure core features of AstroNvim
+    features = {
+      large_buf = { size = 1024 * 500, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+      autopairs = true, -- enable autopairs at start
+      cmp = true, -- enable completion at start
+      diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
+      highlighturl = true, -- highlight URLs at start
+      notifications = true, -- enable notifications at start
+    },
+    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
+    diagnostics = {
+      virtual_text = true,
+      underline = true,
+    },
+    -- vim options can be configured here
+    options = {
+      opt = { -- vim.opt.<key>
+        autoread = true, -- sets vim.opt.autoread
+        autoindent = true,
+        cindent = true,
+        smartindent = true,
+        expandtab = true,
+        confirm = false,
+        cmdheight = 1,
+        joinspaces = false,
+        conceallevel = 3,
+        lazyredraw = false,
+        ttyfast = true,
+        list = true,
+        listchars = {
+            tab = "» ",
+            trail = "·",
+            nbsp = "␣",
+            eol = "↲",
+            extends = "⟩",
+            precedes = "⟨",
+        },
+        matchtime = 2,
+        mouse = 'nv',
+        signcolumn = 'auto',
+        inccommand = 'nosplit',
+        shiftwidth = 2,
+        tabstop = 2,
+        wildmode = 'longest:full,full',
+        relativenumber = true, -- sets vim.opt.relativenumber
+        number = true, -- sets vim.opt.number
+        spell = false, -- sets vim.opt.spell
+        wrap = false, -- sets vim.opt.wrap
+      },
+      g = { -- vim.g.<key>
+        -- configure global vim variables (vim.g)
+        -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
+        -- This can be found in the `lua/lazy_setup.lua` file
+        autoformat_enabled = false,
+        cmp_enabled = true,
+        autopairs_enabled = true,
+        diagnostics_mode = 3,
+        icons_enabled = true,
+        ui_notifications_enabled = true,
+        resession_enabled = false,
+      },
+    },
+    -- Mappings can be configured through AstroCore as well.
+    -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
+    mappings = {
+      -- first key is the mode
+      n = {
+        -- second key is the lefthand side of the map
+
+        -- navigate buffer tabs with `H` and `L`
+        L = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
+        H = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+
+        -- mappings seen under group name "Buffer"
+        ["<Leader>bD"] = {
+          function()
+            require("astroui.status.heirline").buffer_picker(
+              function(bufnr) require("astrocore.buffer").close(bufnr) end
+            )
+          end,
+          desc = "Pick to close",
+        },
+        -- tables with just a `desc` key will be registered with which-key if it's installed
+        -- this is useful for naming menus
+        ["<Leader>b"] = { desc = "Buffers" },
+      },
+      t = {
+        -- setting a mapping to false will disable it
+        -- ["<esc>"] = false,
+      },
+    },
+  },
+}
