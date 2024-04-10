0a1795f8027c531ac32bd4d055cdefc39efa579f
William Smith <william.smith@hensoldt.net>
2024-04-10 22:17:44 +0200

    Indentation updates

--- plugins/text.lua
+++ plugins/text.lua
@@ -1,10 +1,10 @@
 return {
-  -- LaTeX, Typst and markdown integrations
-  { "lervag/vimtex", ft = { "tex", "bib" }, cmd = "VimtexInverseSearch" },
-  { "kaarmu/typst.vim", ft = "typst" },
-  {
-    "iamcco/markdown-preview.nvim",
-    ft = "markdown",
-    build = function() vim.fn["mkdp#util#install"]() end,
-  },
+    -- LaTeX, Typst and markdown integrations
+    { "lervag/vimtex", ft = { "tex", "bib" }, cmd = "VimtexInverseSearch" },
+    { "kaarmu/typst.vim", ft = "typst" },
+    {
+        "iamcco/markdown-preview.nvim",
+        ft = "markdown",
+        build = function() vim.fn["mkdp#util#install"]() end,
+    },
 }
--- plugins/tmux.lua
+++ plugins/tmux.lua
@@ -1,5 +1,5 @@
 return {
-  {
-    "christoomey/vim-tmux-navigator"
-  }
+    {
+        "christoomey/vim-tmux-navigator"
+    }
 }
