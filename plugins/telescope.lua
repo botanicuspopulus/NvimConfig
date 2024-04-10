77ed1941bb76dc498fe10f0b9ef1b07697475768
William Smith <william.smith@hensoldt.net>
2024-04-10 22:17:28 +0200

    Telescope updates. Removing command_palette

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -1,14 +1,9 @@
 return {
   "nvim-telescope/telescope.nvim",
   config = function(plugin, opts)
-    if not opts.extensions then
-      opts.extensions = {
-        "undo",
-        command_palette = {
-          astronvim.user_opts("command_palette", {}),
-        },
-      }
-    end
-    require "plugins.configs.telescope"(plugin, opts)
+    if not opts.extensions then opts.extensions = {
+      "undo",
+    } end
+    require "astronvim.plugins.configs.telescope"(plugin, opts)
   end,
 }
