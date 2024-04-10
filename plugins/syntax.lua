b9b6cd527ae3fbe7b46f06e9f30993bbcca682a7
William Smith <william.smith@hensoldt.net>
2024-04-10 22:15:41 +0200

    syntax plugin updates

--- plugins/syntax.lua
+++ plugins/syntax.lua
@@ -1,19 +1,19 @@
 return {
-  { "styled-components/vim-styled-components", lazy = false },
-  {
-    "folke/todo-comments.nvim",
-    lazy = false,
-    keys = {
-      { "<leader>ft", "<cmd>TodoTelescope<CR>", desc = "Find todos" },
+    { "styled-components/vim-styled-components", lazy = false },
+    {
+        "folke/todo-comments.nvim",
+        lazy = false,
+        keys = {
+            { "<leader>ft", "<cmd>TodoTelescope<CR>", desc = "Find todos" },
+        },
+        dependencies = "nvim-lua/plenary.nvim",
+        opts = {
+            highlight = {
+                pattern = [[.*<(KEYWORDS)\s*]],
+            },
+            search = {
+                pattern = [[\b(KEYWORDS)\b]],
+            },
+        },
     },
-    dependencies = "nvim-lua/plenary.nvim",
-    opts = {
-      highlight = {
-        pattern = [[.*<(KEYWORDS)\s*]],
-      },
-      search = {
-        pattern = [[\b(KEYWORDS)\b]],
-      },
-    },
-  },
 }
