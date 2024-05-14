691136e20508c5196d2923b624700498384f60eb
William Smith <william.smith@hensoldt.net>
2024-05-14 13:59:31 +0200

    Lazy Setup config changes

--- lazy_setup.lua
+++ lazy_setup.lua
@@ -1,31 +1,46 @@
 require("lazy").setup({
-  {
-    "AstroNvim/AstroNvim",
-    version = "^4", -- Remove version tracking to elect for nighly AstroNvim
-    import = "astronvim.plugins",
-    opts = { -- AstroNvim options must be set here with the `import` key
-      mapleader = " ", -- This ensures the leader key must be configured before Lazy is set up
-      maplocalleader = ",", -- This ensures the localleader key must be configured before Lazy is set up
-      icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
-      pin_plugins = nil, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/false to override
+    {
+        "AstroNvim/AstroNvim",
+        version = "^4", -- Remove version tracking to elect for nighly AstroNvim
+        import = "astronvim.plugins",
+        opts = { -- AstroNvim options must be set here with the `import` key
+            mapleader = " ", -- This ensures the leader key must be configured before Lazy is set up
+            maplocalleader = ",", -- This ensures the localleader key must be configured before Lazy is set up
+            icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
+            pin_plugins = nil, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/false to override
+        },
     },
-  },
-  { import = "community" },
-  { import = "plugins" },
+    { import = "community" },
+    { import = "plugins" },
 } --[[@as LazySpec]], {
-  -- Configure any other `lazy.nvim` configuration options here
-  install = { colorscheme = { "tokyonight-night", "astrodark", "habamax" } },
-  ui = { backdrop = 100 },
-  performance = {
-    rtp = {
-      -- disable some rtp plugins, add more to your liking
-      disabled_plugins = {
-        "gzip",
-        "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "zipPlugin",
-      },
-    },
-  },
-} --[[@as LazyConfig]])
+        -- Configure any other `lazy.nvim` configuration options here
+        install = { colorscheme = { "tokyonight", "astrodark", "habamax" } },
+        ui = { backdrop = 100 },
+        performance = {
+            rtp = {
+                -- disable some rtp plugins, add more to your liking
+                disabled_plugins = {
+                    "2html_plugin",
+                    "getscript",
+                    "getscriptPlugin",
+                    "logipat",
+                    "netrw",
+                    "netrwFileHandlers",
+                    "loaded_remote_plugins",
+                    "loaded_tutor_mode_plugin",
+                    "netrwSettings",
+                    "rrhelper",
+                    "spellfile_plugin",
+                    "tohtml",
+                    "gzip",
+                    "matchit",
+                    "tutor",
+                    "zipPlugin",
+                    "netrwPlugin",
+                    "tar",
+                    "tarPlugin",
+                    "matchparen"
+                },
+            },
+        },
+    } --[[@as LazyConfig]])
