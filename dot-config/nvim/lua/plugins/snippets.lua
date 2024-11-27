a6416bd706251362b3cc9673819832e96305e68b
William Smith <wsmith1@csir.co.za>
2024-11-27 16:57:02 +0200

    feat: update LuaSnip configuration formatting
    - Reformatted LuaSnip configuration for better readability
    - Changed indentation from tabs to spaces
    - Ensured consistent use of double quotes for strings
    - No functional changes made to the configuration

--- dot-config/nvim/lua/plugins/snippets.lua
+++ dot-config/nvim/lua/plugins/snippets.lua
@@ -1,23 +1,23 @@
 return {
-	{
-		'L3MON4D3/LuaSnip',
-		dependencies = { "rafamadriz/friendly-snippets" },
-		config = function(plugin, opts)
-			local ls = require 'luasnip'
-			require('astronvim.plugins.configs.luasnip')(plugin, opts)
-			require('luasnip/loaders/from_vscode').lazy_load( { paths = { './lua/snippets' } } )
-			ls.config.set_config {
-				-- This tells LuaSnip to remember to keep around the last snippet.
-				-- You can jump back into it even if you move outside of the selection
-				history = true,
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function(plugin, opts)
+      local ls = require "luasnip"
+      require "astronvim.plugins.configs.luasnip"(plugin, opts)
+      require("luasnip/loaders/from_vscode").lazy_load { paths = { "./lua/snippets" } }
+      ls.config.set_config {
+        -- This tells LuaSnip to remember to keep around the last snippet.
+        -- You can jump back into it even if you move outside of the selection
+        history = true,
 
-				-- This one is cool since if you have dynamic snippets, it updates as you type!
-				updateevents = "TextChanged,TextChangedI",
-				enable_autosnippets = false
-			}
-			ls.filetype_extend("javascriptreact", { "javascript" })
-			ls.filetype_extend("typescript", {"javascript"})
-			ls.filetype_extend("typescriptreact", {"javascript"})
-		end,
-	}
+        -- This one is cool since if you have dynamic snippets, it updates as you type!
+        updateevents = "TextChanged,TextChangedI",
+        enable_autosnippets = false,
+      }
+      ls.filetype_extend("javascriptreact", { "javascript" })
+      ls.filetype_extend("typescript", { "javascript" })
+      ls.filetype_extend("typescriptreact", { "javascript" })
+    end,
+  },
 }
