79429ef9c9fe39c183a6d5ade3e4242f2863f8f8
William Smith <wsmith1@csir.co.za>
2024-12-02 12:26:50 +0200

    feat(code_ai): adding codeium configuration
    Adding Codeium plugin for the eventual replacement of GitHub Copilot

---
+++ dot-config/nvim/lua/plugins/code_ai.lua
@@ -0,0 +1,54 @@
+return {
+  "Exafunction/codeium.nvim",
+  event = "User AstroFile",
+  cmd = "Codeium",
+  opts = {
+    enable_chat = true,
+    virtual_text = {
+      enabled = true,
+      manual = false,
+      filetypes = {},
+      default_filetype_enabled = true,
+      idle_delay = 75,
+      virtual_text_priority = 65535,
+      map_keys = true,
+      accept_fallback = nil,
+      keybindings = {
+        accept = "<M-a>",
+        accept_word = "<M-f>",
+        accept_line = false,
+        clear = false,
+        next = "<M-]>",
+        prev = "<M-[>",
+      }
+    }
+  },
+  dependencies = {
+    { "nvim-lua/plenary.nvim" },
+    { "hrsh7th/nvim-cmp" },
+    { "AstroNvim/astroui" ,
+      opts = {
+        icons = {
+          Codeium = " ",
+        },
+      },
+    },
+    { "AstroNvim/astrocore",
+      opts = function(_, opts)
+        return require("astrocore").extend_tbl(opts, {
+          mappings = {
+            n = {
+              ["<Leader>;"] = {
+                desc = require("astroui").get_icon("Codeium", 1, true) .. "Codeium",
+              },
+              ["<Leader>;o"] = {
+                desc = "Open Chat",
+                function() vim.cmd "Codeium Chat" end,
+              },
+            },
+          },
+        })
+      end,
+    },
+  },
+}
