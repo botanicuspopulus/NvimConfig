fc11366250827086224071adb1a449e34dd74015
William Smith <wsmith1@csir.co.za>
2025-04-16 09:42:08 +0000

    refactor(copilot): Simplifying and refining copilot configuration to work with blink.cmp

--- dot-config/nvim/lua/plugins/copilot.lua
+++ dot-config/nvim/lua/plugins/copilot.lua
@@ -3,41 +3,33 @@ return {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
     build = ":Copilot Auth",
-    event = "InsertEnter",
+    event = "BufReadPost",
     opts = {
       suggestion = {
-        enabled = false,
+        auto_trigger = true,
+        debounce = 150,
         keymap = {
           accept = false,
-          next = "<M-]>",
-          prev = "<M-[>",
-          dismiss = "<C-]>",
         },
-      },
-      panel = { enabled = false },
-      filetypes = {
-        markdown = false,
-        help = false,
       },
     },
     specs = {
-      "AstroNvim/astrocore",
-      opts = {
-        options = {
-          g = {
-            ai_accept = function()
-              if require("copilot.suggestion").is_visible() then
-                require("copilot.suggestion").accept()
-                return true
-              end
-            end,
+      {
+        "AstroNvim/astrocore",
+        opts = {
+          options = {
+            g = {
+              ai_accept = function()
+                local copilot_suggestion = require "copilot.suggestion"
+                if copilot_suggestion.is_visible() then
+                  copilot_suggestion.accept()
+                  return true
+                end
+              end,
+            },
           },
         },
       },
     },
-  },
-  {
-    "zbirenbaum/copilot-cmp",
-    opts = {},
   },
 }
