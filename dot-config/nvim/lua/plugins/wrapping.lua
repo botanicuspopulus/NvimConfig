e0b4da6e46427b77d3d62973686cc5144c169efc
William Smith <william@williamgsmith.co.za>
2025-05-27 11:38:17 +0200

    feat(wrapping): small QoL changes

--- dot-config/nvim/lua/plugins/wrapping.lua
+++ dot-config/nvim/lua/plugins/wrapping.lua
@@ -1,29 +1,19 @@
-local opts = {
-  auto_set_mode_filetype_allowlist = {
-    "asciidoc",
-    "gitcommit",
-    "latex",
-    "mail",
-    "markdown",
-    "rst",
-    "tex",
-    "text",
-  },
-}
-
 return {
   {
     "andrewferrier/wrapping.nvim",
     opts = {
+      notify_on_switch = false,
       auto_set_mode_filetype_allowlist = {
         "asciidoc",
         "gitcommit",
+        "help",
         "latex",
         "mail",
         "markdown",
         "rst",
         "tex",
         "text",
+        "typst",
       },
     },
   },
