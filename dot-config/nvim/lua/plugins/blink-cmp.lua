806a7ee41a566516ff104b7c93541d3178d5c48f
William Smith <wsmith1@csir.co.za>
2025-10-15 17:47:33 +0200

    feat: adding blink-cmp

---
+++ dot-config/nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,81 @@
+return {
+  {
+    "saghen/blink.cmp",
+    version = "*",
+    build = "cargo build --release",
+    opts_extend = {
+      "sources.completion.enabled_providers",
+      "sources.compat",
+      "sources.default",
+    },
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      "L3MON4D3/LuaSnip",
+      {
+        "saghen/blink.compat",
+        optional = true,
+        opts = {},
+        version = "*",
+      },
+    },
+    event = { "InsertEnter", "CmdlineEnter" },
+    opts = {
+      snippets = { preset = 'luasnip' },
+      appearance = {
+        use_nvim_cmp_as_default = false,
+        nerd_font_variant = "mono",
+      },
+      completion = {
+        accept = {
+          auto_brackets = { enabled = true },
+          menu = { draw = { treesitter = { "lsp" } } },
+          documentation = {
+            auto_show = true,
+            auto_show_delay_ms = 200,
+          },
+        },
+      },
+      sources = {
+        compat = {},
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+      cmdline = {
+        enabled = true,
+        keymap = {
+          preset = "cmdline",
+          ["<Right>"] = false,
+          ["<Left>"] = false,
+        },
+        completion = {
+          list = { selection = { preselect = false } },
+          menu = {
+            auto_show = function(ctx) return vim.fn.getcmdtype() == ":" end,
+          },
+          ghost_text = { enabled = true },
+        },
+      },
+
+      keymap = {
+        preset = "enter",
+        ["<C-y>"] = { "select_and_accept" },
+      },
+    },
+    config = function(_, opts)
+      local enabled = opts.sources.default
+
+      for _, source in ipairs(opts.sources.compat or {}) do
+        opts.sources.providers[source] = vim.tbl_deep_extend(
+          "force",
+          { name = source, module = "blink.compat.source" },
+          opts.sources.providers[source] or {}
+        )
+
+        if type(enabled) == "table" and not vim.tbl_contains(enabled, source) then table.insert(enabled, source) end
+      end
+
+      opts.sources.compat = nil
+
+      require("blink.cmp").setup(opts)
+    end,
+  },
+}
