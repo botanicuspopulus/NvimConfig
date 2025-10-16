865fd82267806403dfcfb183fee702c1cade2931
William Smith <william@williamgsmith.co.za>
2025-10-16 16:24:23 +0200

    refactor: updating blink-cmp configuration
    - Adding options to add borders to the completion windows.
    - Added new sources including ripgrep, datword, and nerdfont.

--- dot-config/nvim/lua/plugins/blink-cmp.lua
+++ dot-config/nvim/lua/plugins/blink-cmp.lua
@@ -1,3 +1,13 @@
+local source_priority = {
+  lsp = 1,
+  snippets = 2,
+  path = 3,
+  buffer = 4,
+  ripgrep = 5,
+  datword = 6,
+  nerdfont = 7,
+}
+
 return {
   {
     "saghen/blink.cmp",
@@ -11,6 +21,9 @@ return {
     dependencies = {
       "rafamadriz/friendly-snippets",
       "L3MON4D3/LuaSnip",
+      "xieyonn/blink-cmp-dat-word",
+      "MahanRahmati/blink-nerdfont.nvim",
+      { "mikavilpas/blink-ripgrep.nvim", version = "*" },
       {
         "saghen/blink.compat",
         optional = true,
@@ -20,7 +33,7 @@ return {
     },
     event = { "InsertEnter", "CmdlineEnter" },
     opts = {
-      snippets = { preset = 'luasnip' },
+      snippets = { preset = "luasnip" },
       appearance = {
         use_nvim_cmp_as_default = false,
         nerd_font_variant = "mono",
@@ -29,34 +42,114 @@ return {
         accept = {
           auto_brackets = { enabled = true },
         },
-        menu = { draw = { treesitter = { "lsp" } } },
+        menu = {
+          draw = {
+            treesitter = { "lsp" },
+            columns = {
+              { 'kind_icon' },
+              { 'label', 'label_description', gap = 1 },
+              { 'kind' },
+            },
+            components = {
+              kind_icon = {
+                text = function(ctx)
+                  local icon = ctx.kind_icon
+
+                  if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                    local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+                    if dev_icon then icon = dev_icon end
+                  else
+                    icon = require("lspkind").symbolic(ctx.kind, {
+                      mode = "symbol",
+                    })
+                  end
+
+                  return icon .. ctx.icon_gap
+                end,
+                highlight = function(ctx)
+                  local hl = ctx.kind_hl
+
+                  if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                    local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+                    if dev_icon then hl = dev_hl end
+                  end
+
+                  return hl
+                end,
+              },
+            },
+          },
+          border = "single",
+        },
         documentation = {
           auto_show = true,
           auto_show_delay_ms = 200,
+          window = { border = "single" },
+        },
+      },
+      signature = { window = { border = "single" } },
+      fuzzy = {
+        implementation = 'prefer_rust_with_warning',
+        sorts = {
+          function(a, b)
+            local a_priority = source_priority[a.source_id]
+            local b_priority = source_priority[b.source_id]
+
+            if a_priority ~= b_priority then return a_priority < b_priority end
+          end,
+          "score",
+          "sort_text",
         },
       },
       sources = {
         compat = {},
-        default = { "lsp", "path", "snippets", "buffer" },
+        default = { "lsp", "snippets", "path", "buffer", "ripgrep", "datword", "nerdfont" },
+        min_keyword_length = function() return vim.bo.filetype == "markdown" and 2 or 0 end,
         providers = {
           lsp = {
             timeout_ms = 500,
             max_items = 10,
-            min_keyword_length = 2,
             async = true,
           },
           buffer = {
             max_items = 10,
-            min_keyword_length = 4,
+            opts = {
+              get_bufnrs = function()
+                return vim.tbl_filter(function(bufnr) return vim.bo[bufnr].buftype == "" end, vim.api.nvim_list_bufs())
+              end,
+            },
+          },
+          ripgrep = {
+            module = "blink-ripgrep",
+            name = "Ripgrep",
+            ---@module "blink-ripgrep"
+            ---@type blink-ripgrep.Options
+            opts = {
+              backend = { use = "gitgrep-or-ripgrep" },
+            },
+            max_items = 10,
           },
           path = {
             max_items = 5,
           },
           snippets = {
             max_items = 8,
-            min_keyword_length = 3,
-            should_show_items = function(ctx) return true end,
-          }
+            should_show_items = function(ctx) return ctx.trigger.initial_kind ~= "trigger_character" end,
+          },
+          datword = {
+            name = "Word",
+            module = "blink-cmp-dat-word",
+            opts = {
+              paths = {
+                vim.fn.stdpath "config" .. "/spell/words_alpha.txt",
+              },
+            },
+          },
+          nerdfont = {
+            module = "blink-nerdfont",
+            name = "Nerd Fonts",
+            opts = { insert = true },
+          },
         },
       },
       cmdline = {
--- dot-config/nvim/lua/plugins/lsp.lua
+++ dot-config/nvim/lua/plugins/lsp.lua
@@ -53,6 +53,40 @@ return {
         },
         servers = {
           stylua = { enabled = false },
+          clangd = {
+            keys = {
+              { '<leader>ch', '<cmd>ClangdSwitchSourceHeader<cr>', desc = 'Switch Source/Header (C/C++)' },
+            },
+            root_markers = {
+              'compile_commands.json',
+              'compile_flags.txt',
+              'configure.ac',
+              'Makefile',
+              'configure.in',
+              'config.h.in',
+              'meson.build',
+              'meson_options.txt',
+              'build.ninja',
+              '.git',
+            },
+            capabilities = {
+              offsetEncoding = { 'utf-16' },
+            },
+            cmd = {
+              'clangd',
+              '--background-index',
+              '--clang-tidy',
+              '--header-insertion=iwyu',
+              '--completion-style=detailed',
+              '--function-arg-placeholders',
+              '--fallback-style=llvm',
+            },
+            init_options = {
+              usePlaceHolders = true,
+              completeUnimported = true,
+              clangFileStatus = true,
+            },
+          },
           lua_ls = {
             settings = {
               Lua = {
