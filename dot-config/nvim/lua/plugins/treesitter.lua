f99dce079451583c6dc448c195073f59fdb79bc4
William Smith <william@williamgsmith.co.za>
2025-10-17 18:18:51 +0200

    style: formatting file

--- dot-config/nvim/lua/plugins/treesitter.lua
+++ dot-config/nvim/lua/plugins/treesitter.lua
@@ -1,138 +1,141 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		event = { "VeryLazy" },
-		cmd = { "TSUpdate", "TSInstall", "TSLog", "TSUninstall" },
-		build = ":TSUpdate",
-		opts = {
-			ensure_installed = {
-				"bash",
-				"c",
-				"cpp",
-				"diff",
-				"html",
-				"javascript",
-				"jsdoc",
-				"json",
-				"jsonc",
-				"latex",
-				"lua",
-				"luadoc",
-				"luap",
-				"markdown",
-				"markdown_inline",
-				"printf",
-				"python",
-				"regex",
-				"toml",
-				"tsx",
-				"typescript",
-				"vim",
-				"vimdoc",
-				"xml",
-				"yaml",
-			},
-			auto_install = true,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = { enable = true },
-			folds = { enable = true },
-			config = function(_, opts)
-				require("nvim-treesitter.configs").setup(opts)
-			end,
-		},
-	},
-	{
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		event = "VeryLazy",
-		branch = "main",
-		opts = {
-			select = {
-				enable = true,
-				lookahead = true,
-				keymaps = {
-					["af"] = "@function.outer",
-					["if"] = "@function.inner",
-					["ac"] = "@class.outer",
-					["ic"] = "@class.inner",
-					["aa"] = "@parameter.outer",
-					["ia"] = "@parameter.inner",
-				},
-			},
-			move = {
-				enable = true,
-				set_jumps = true,
-				keys = {
-					goto_next_start = {
-						["]f"] = "@function.outer",
-						["]c"] = "@class.outer",
-						["]a"] = "@parameter.inner",
-					},
-					goto_next_end = {
-						["]F"] = "@function.outer",
-						["]C"] = "@class.outer",
-						["]A"] = "@parameter.inner",
-					},
-					goto_previous_start = {
-						["[f"] = "@function.outer",
-						["[c"] = "@class.outer",
-						["[a"] = "@parameter.inner",
-					},
-					goto_previous_end = {
-						["[F"] = "@function.outer",
-						["[C"] = "@class.outer",
-						["[A"] = "@parameter.inner",
-					},
-				},
-			},
-		},
-		config = function(_, opts)
-			vim.api.nvim_create_autocmd("FileType", {
-				group = vim.api.nvim_create_augroup("nvim_treesitter_textobjects", { clear = true }),
-				callback = function(ev)
-					local moves = vim.tbl_get(opts, "move", "keys") or {}
-
-					for method, keymaps in pairs(moves) do
-						for key, query in pairs(keymaps) do
-							local desc = query:gsub("@", ""):gsub("%..*", "")
-							desc = desc:sub(1, 1):upper() .. desc:sub(2)
-							desc = (key:sub(1, 1) == "[" and "Prev " or "Next ") .. desc
-							desc = desc .. (key:sub(2, 2) == key:sub(2, 2):upper() and " End" or " Start")
-
-							if not (vim.wo.diff and key:find("[cC]")) then
-								vim.keymap.set({ "n", "x", "o" }, key, function() 
-                  require('nvim-treesitter-textobjects.move')[method](query, 'textobjects')
-                end, {
-									buffer = ev.buf,
-									desc = desc,
-									silent = true,
-								})
-							end
-						end
-					end
-				end,
-			})
-		end,
-	},
   {
-    'nvim-treesitter/nvim-treesitter-context',
-    after = 'nvim-treesitter',
+    "nvim-treesitter/nvim-treesitter",
+    event = { "VeryLazy" },
+    cmd = { "TSUpdate", "TSInstall", "TSLog", "TSUninstall" },
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "c",
+        "cpp",
+        "diff",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "jsonc",
+        "latex",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "ninja",
+        "printf",
+        "python",
+        "regex",
+        "rst",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
+      },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = { enable = true },
+      folds = { enable = true },
+      config = function(_, opts) require("nvim-treesitter.configs").setup(opts) end,
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    event = "VeryLazy",
+    branch = "main",
+    opts = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = "@class.inner",
+          ["aa"] = "@parameter.outer",
+          ["ia"] = "@parameter.inner",
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true,
+        keys = {
+          goto_next_start = {
+            ["]f"] = "@function.outer",
+            ["]c"] = "@class.outer",
+            ["]a"] = "@parameter.inner",
+          },
+          goto_next_end = {
+            ["]F"] = "@function.outer",
+            ["]C"] = "@class.outer",
+            ["]A"] = "@parameter.inner",
+          },
+          goto_previous_start = {
+            ["[f"] = "@function.outer",
+            ["[c"] = "@class.outer",
+            ["[a"] = "@parameter.inner",
+          },
+          goto_previous_end = {
+            ["[F"] = "@function.outer",
+            ["[C"] = "@class.outer",
+            ["[A"] = "@parameter.inner",
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("nvim_treesitter_textobjects", { clear = true }),
+        callback = function(ev)
+          local moves = vim.tbl_get(opts, "move", "keys") or {}
+
+          for method, keymaps in pairs(moves) do
+            for key, query in pairs(keymaps) do
+              local desc = query:gsub("@", ""):gsub("%..*", "")
+              desc = desc:sub(1, 1):upper() .. desc:sub(2)
+              desc = (key:sub(1, 1) == "[" and "Prev " or "Next ") .. desc
+              desc = desc .. (key:sub(2, 2) == key:sub(2, 2):upper() and " End" or " Start")
+
+              if not (vim.wo.diff and key:find "[cC]") then
+                vim.keymap.set(
+                  { "n", "x", "o" },
+                  key,
+                  function() require("nvim-treesitter-textobjects.move")[method](query, "textobjects") end,
+                  {
+                    buffer = ev.buf,
+                    desc = desc,
+                    silent = true,
+                  }
+                )
+              end
+            end
+          end
+        end,
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    after = "nvim-treesitter",
     config = function()
-      require('treesitter-context').setup({
+      require("treesitter-context").setup {
         enable = true,
         multiwindow = false,
         max_lines = 0,
         min_window_height = 0,
         line_numbers = true,
         multiline_threshold = 20,
-        trim_scope = 'outer',
-        mode = 'cursor',
+        trim_scope = "outer",
+        mode = "cursor",
         separator = nil,
         zindex = 20,
-        on_attach = nil
-      })
+        on_attach = nil,
+      }
     end,
   },
   {
@@ -141,8 +144,8 @@ return {
     opts = {},
   },
   {
-    'folke/ts-comments.nvim',
-    event = 'VeryLazy',
+    "folke/ts-comments.nvim",
+    event = "VeryLazy",
     opts = {},
-  }
+  },
 }
