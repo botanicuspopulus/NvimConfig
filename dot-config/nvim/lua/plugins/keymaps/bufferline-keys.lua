f35fff2fca78cfcf7210b3e57d71353cf2653cd1
William Smith <william@williamgsmith.co.za>
2025-10-17 18:12:56 +0200

    refactor: moving keybinds for bufferline to lua/plugins/keymaps

---
+++ dot-config/nvim/lua/plugins/keymaps/bufferline-keys.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+		keys = {
+			{ "<leader>bp", "<cmd>BufferLineTogglePin<CR>", desc = "Buffer: Toggle Pin" },
+			{ "<leader>bP", "<cmd>BufferLineGroupClose ungrouped<CR>", desc = "Buffer: Delete Non-Pinned Buffers" },
+			{ "<leader>br", "<cmd>BufferLineCloseRight<CR>", desc = "Buffer: Delete Buffers to the Right" },
+			{ "<leader>bl", "<cmd>BufferLineCloseLeft<CR>", desc = "Buffer: Delete Buffers to the Left" },
+			{ "<S-h>", "<cmd>BufferLineCyclePrev<CR>", desc = "Buffer: Previous Buffer" },
+			{ "<S-l>", "<cmd>BufferLineCycleNext<CR>", desc = "Buffer: Next Buffer" },
+			{ "[b", "<cmd>BufferLineCyclePrev<CR>", desc = "Buffer: Previous Buffer" },
+			{ "]b", "<cmd>BufferLineCycleNext<CR>", desc = "Buffer: Next Buffer" },
+			{ "[B", "<cmd>BufferLineMovePrev<CR>", desc = "Buffer: Move Buffer Previous" },
+			{ "]B", "<cmd>BufferLineMoveNext<CR>", desc = "Buffer: Move Buffer Next" },
+		},
+  }
+}
