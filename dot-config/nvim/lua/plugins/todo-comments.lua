1f060bbd331a14c61c39a7633889b9570810d94a
William Smith <william@williamgsmith.co.za>
2025-10-17 18:15:06 +0200

    refactor: moving keybinds for todo-comments to lua/plugins/keymaps

--- dot-config/nvim/lua/plugins/todo-comments.lua
+++ dot-config/nvim/lua/plugins/todo-comments.lua
@@ -4,9 +4,5 @@ return {
     event = "VeryLazy",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {},
-    keys = {
-      { ']t', function() require('todo-comments').jump_next() end, desc = 'Next Todo Comment' },
-      { '[t', function() require('todo-comments').jump_prev() end, desc = 'Previous Todo Comment' },
-    }
   },
 }
