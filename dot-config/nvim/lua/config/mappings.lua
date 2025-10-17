32d2fb5e645371ee7b3c1feaab801efc2c84bda6
William Smith <william@williamgsmith.co.za>
2025-10-17 18:22:34 +0200

    refactor: breaking larger keymapping table into smaller keymapping tables by logical grouping

--- dot-config/nvim/lua/config/mappings.lua
+++ dot-config/nvim/lua/config/mappings.lua
@@ -1,35 +1,124 @@
-local map = vim.keymap.set
+local keymap = vim.keymap.set
 
-map('n', '<leader>qq', '<cmd>qa<cr>', { desc = 'Quit'})
+local motion_keybinds = {
+  -- Motion
+  {
+    { "n", "x" },
+    "j",
+    "v:count == 0 ? 'gj' : 'j'",
+    { desc = "Down", expr = true, silent = true },
+  },
+  {
+    { "n", "x" },
+    "k",
+    "v:count == 0 ? 'gk' : 'k'",
+    { desc = "Up", expr = true, silent = true },
+  },
+  {
+    { "n" },
+    "<C-h>",
+    "<C-w>h",
+    { desc = "Window: Go Left", remap = true },
+  },
+  {
+    { "n" },
+    "<C-j>",
+    "<C-w>j",
+    { desc = "Window: Go Down", remap = true },
+  },
+  {
+    { "n" },
+    "<C-k>",
+    "<C-w>k",
+    { desc = "Window: Go Up", remap = true },
+  },
+  {
+    { "n" },
+    "<C-l>",
+    "<C-w>l",
+    { desc = "Window: Go Right", remap = true },
+  },
+}
 
-map({ 'n', 'x' }, 'j', "v:count == 0 ? 'gj' : 'j'", { desc = 'Down', expr = true, silent = true })
-map({ 'n', 'x' }, 'k', "v:count == 0 ? 'gk' : 'k'", { desc = 'Up', expr = true, silent = true })
+local buffer_keybinds = {
+  { "n", "<leader>bb", "<cmd>e #<cr>", { desc = "Buffer: Switch to other" } },
+  { "n", "<leader>bo", function() require("snacks").bufdelete.other() end, { desc = "Buffer: Delete Other" } },
+  {
+    "n",
+    "<leader>bD",
+    "<cmd>:bd<cr>",
+    { desc = "Buffer: Delete Buffer and Window" },
+  },
+}
 
-map({ 'n' }, "<C-h>", "<C-w>h", { desc = "Window: Go Left", remap = true })
-map({ 'n' }, "<C-j>", "<C-w>j", { desc = "Window: Go Down", remap = true })
-map({ 'n' }, "<C-k>", "<C-w>k", { desc = "Window: Go Up", remap = true })
-map({ 'n' }, "<C-l>", "<C-w>l", { desc = "Window: Go Right", remap = true })
+local window_keybinds = {
+  -- windows
+  {
+    "n",
+    "<leader>-",
+    "<C-W>s",
+    group = "window",
+    { desc = "Window: Split Below", remap = true },
+  },
+  {
+    "n",
+    "<leader>ws",
+    "<C-W>s",
+    group = "window",
+    { desc = "Window: Split Below", remap = true },
+  },
+  {
+    "n",
+    "<leader>|",
+    "<C-W>v",
+    group = "window",
+    { desc = "Window: Split Right", remap = true },
+  },
+  {
+    "n",
+    "<leader>wv",
+    "<C-W>v",
+    group = "window",
+    { desc = "Window: Split Right", remap = true },
+  },
+  {
+    "n",
+    "<leader>wd",
+    "<C-W>c",
+    group = "window",
+    { desc = "Window: Delete", remap = true },
+  },
+}
 
-map('n', '<leader>bb', '<cmd>e #<cr>', { desc = "Buffer: Switch to other" })
-map('n', '<leader>bo', function()
-  Snacks.bufdelete.other()
-end, { desc = "Buffer: Delete Other" })
-map('n', '<leader>bD', '<cmd>:bd<cr>', { desc = "Buffer: Delete Buffer and Window" })
+local tab_keybinds = {
+  -- tabs
+  { "n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Tab: Last" } },
+  { "n", "<leader><tab>o", "<cmd>tabonly<cr>", { desc = "Tab: Close Other" } },
+  { "n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "Tab: First" } },
+  { "n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "Tab: New" } },
+  { "n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Tab: Next" } },
+  { "n", "<leader><tab>[", "<cmd>tabprev<cr>", { desc = "Tab: Previous" } },
+  { "n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Tab: Close" } },
+}
 
--- better indenting
-map('v', '<', '<gv')
-map('v', '>', '>gv')
+local general_keybinds = {
+  { "n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit" } },
 
--- windows
-map('n', '<leader>-', '<C-W>s', { desc = 'Window: Split Below', remap = true })
-map('n', '<leader>|', '<C-W>v', { desc = 'Window: Split Right', remap = true })
-map('n', '<leader>wd', '<C-W>c', { desc = "Window: Delete", remap = true })
+  -- better indenting
+  { "v", "<", "<gv" },
+  { "v", ">", ">gv" },
+}
 
-map('n', '<leader><tab>l', "<cmd>tablast<cr>", { desc = 'Tab: Last' })
-map('n', '<leader><tab>o', '<cmd>tabonly<cr>', { desc = 'Tab: Close Other' })
-map('n', '<leader><tab>f', '<cmd>tabfirst<cr>', { desc = 'Tab: First' })
-map('n', '<leader><tab><tab>', '<cmd>tabnew<cr>', { desc = 'Tab: New' })
-map('n', '<leader><tab>]', '<cmd>tabnext<cr>', { desc = 'Tab: Next' })
-map('n', '<leader><tab>[', '<cmd>tabprev<cr>', { desc = 'Tab: Previous' })
-map('n', '<leader><tab>d', '<cmd>tabclose<cr>', { desc = 'Tab: Close' })
+local all_keybinds = {}
 
+local extend_keybinds = function(keybind_table) vim.list_extend(all_keybinds, keybind_table) end
+
+extend_keybinds(motion_keybinds)
+extend_keybinds(buffer_keybinds)
+extend_keybinds(window_keybinds)
+extend_keybinds(tab_keybinds)
+extend_keybinds(general_keybinds)
+
+for _, keybind in ipairs(all_keybinds) do
+  keymap(unpack(keybind))
+end
