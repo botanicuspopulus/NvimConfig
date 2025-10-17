e812ee6749e9372d207881efeeafa1cbfd7c834a
William Smith <william@williamgsmith.co.za>
2025-10-17 18:23:24 +0200

    fix: adding plugins.lsp and plugins.keymaps to lazy spec to ensure that they are imported

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -17,7 +17,11 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	spec = { { import = "plugins" } },
+	spec = { 
+    { import = "plugins" },
+    { import = "plugins.lsp" },
+    { import = "plugins.keymaps" },
+  },
 	install = { colorscheme = { "catppuccin-macchiato" } },
   performance = {
     rtp = {
