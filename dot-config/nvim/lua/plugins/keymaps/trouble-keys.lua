7d592801ee67cdb483e71913f5389e3ae57e3989
William Smith <wsmith1@csir.co.za>
2025-10-21 13:06:25 +0200

    refactor(trouble): creating variable for trouble prefix

--- dot-config/nvim/lua/plugins/keymaps/trouble-keys.lua
+++ dot-config/nvim/lua/plugins/keymaps/trouble-keys.lua
@@ -1,16 +1,53 @@
+local trouble_prefix = "<leader>x"
 return {
   {
     "folke/trouble.nvim",
     keys = {
-      { '<leader>xx', '<cmd>Trouble diagnostics toggle<cr>', desc = "Trouble: Diagnostics" },
-      { '<leader>xX', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', desc = 'Trouble: Buffer Diagnostics (Trouble)'},
-      { '<leader>xs', '<cmd>Trouble symbols toggle focus=false<cr>', desc = 'Trouble: Symbols' },
-      { '<leader>xl', '<cmd>Trouble lsp toggle focus=false win.position=right<cr>', desc = 'Trouble: LSP Definitions/References' },
-      { '<leader>xL', '<cmd>Trouble loclist toggle<cr>', desc = 'Trouble: Location List' },
-      { '<leader>xQ', '<cmd>Trouble qflist toggle<cr>', desc = 'Trouble: Quickfix List' },
-      { '<leader>xe', '<cmd>Trouble diagnostics toggle focus=false filter.severity=vim.diagnostic.severity.ERROR<cr>', desc = 'Trouble: Errors' },
-      { '<leader>xw', '<cmd>Trouble diagnostics toggle focus=false filter.severity=vim.diagnostic.severity.WARN<cr>', desc = 'Trouble: Warnings'},
-      { '<leader>xi', '<cmd>Trouble diagnostics toggle focus=false filter.severity=vim.diagnostic.severity.INFO<cr>', desc = 'Trouble: Info' },
+      {
+        trouble_prefix .. "x",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Trouble: Diagnostics",
+      },
+      {
+        trouble_prefix .. "X",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Trouble: Buffer Diagnostics (Trouble)",
+      },
+      {
+        trouble_prefix .. "s",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Trouble: Symbols",
+      },
+      {
+        trouble_prefix .. "l",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "Trouble: LSP Definitions/References",
+      },
+      {
+        trouble_prefix .. "L",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Trouble: Location List",
+      },
+      {
+        trouble_prefix .. "Q",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Trouble: Quickfix List",
+      },
+      {
+        trouble_prefix .. "e",
+        "<cmd>Trouble diagnostics toggle focus=false filter.severity=vim.diagnostic.severity.ERROR<cr>",
+        desc = "Trouble: Errors",
+      },
+      {
+        trouble_prefix .. "w",
+        "<cmd>Trouble diagnostics toggle focus=false filter.severity=vim.diagnostic.severity.WARN<cr>",
+        desc = "Trouble: Warnings",
+      },
+      {
+        trouble_prefix .. "i",
+        "<cmd>Trouble diagnostics toggle focus=false filter.severity=vim.diagnostic.severity.INFO<cr>",
+        desc = "Trouble: Info",
+      },
     },
   },
 }
