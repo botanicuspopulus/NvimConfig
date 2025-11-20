2129d07d1ef3d7eb1912961b2c3ab041aad5f1fc
William Smith <wsmith1@csir.co.za>
2025-11-20 14:46:58 +0200

    feat: adding supermaven plugin

---
+++ dot-config/nvim/lua/plugins/supermaven.lua
@@ -0,0 +1,10 @@
+return {
+  "supermaven-inc/supermaven-nvim",
+  opts = {
+    keymaps = {
+      accept_suggestion = "<C-y>",
+      clear_suggestion = "<C-]>",
+      accept_word = "<C-j>",
+    },
+  },
+}
