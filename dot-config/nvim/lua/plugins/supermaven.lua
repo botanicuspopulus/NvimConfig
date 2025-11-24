59873e81df9d105dd8532b98a069329e05744c39
William Smith <wsmith1@csir.co.za>
2025-11-24 09:33:11 +0200

    fix: changing supermaven accept to C-l

--- dot-config/nvim/lua/plugins/supermaven.lua
+++ dot-config/nvim/lua/plugins/supermaven.lua
@@ -1,10 +1,12 @@
 return {
   "supermaven-inc/supermaven-nvim",
-  opts = {
-    keymaps = {
-      accept_suggestion = "<C-y>",
-      clear_suggestion = "<C-]>",
-      accept_word = "<C-j>",
-    },
-  },
+  config = function()
+    require("supermaven-nvim").setup {
+      keymaps = {
+        accept_suggestion = "<C-l>",
+        clear_suggestion = "<C-]>",
+        accept_word = "<C-j>",
+      },
+    }
+  end,
 }
