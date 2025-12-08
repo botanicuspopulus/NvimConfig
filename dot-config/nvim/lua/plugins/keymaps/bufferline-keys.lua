cf19e778a6c3b5cfee7e0be9a8fa4d5a4b724806
William Smith <wsmith1@csir.co.za>
2025-12-08 12:14:45 +0200

    refactor: updating bufferline for handling buffers and removing tab.lua in favour of bufferline

--- dot-config/nvim/lua/plugins/keymaps/bufferline-keys.lua
+++ dot-config/nvim/lua/plugins/keymaps/bufferline-keys.lua
@@ -1,15 +1,35 @@
+local leader = "<leader>b"
+
 return {
   {
     "akinsho/bufferline.nvim",
-		keys = {
-			{ "<leader>bp", "<cmd>BufferLineTogglePin<CR>", desc = "Buffer: Toggle Pin" },
-			{ "<leader>bP", "<cmd>BufferLineGroupClose ungrouped<CR>", desc = "Buffer: Delete Non-Pinned Buffers" },
-			{ "<leader>br", "<cmd>BufferLineCloseRight<CR>", desc = "Buffer: Delete Buffers to the Right" },
-			{ "<leader>bl", "<cmd>BufferLineCloseLeft<CR>", desc = "Buffer: Delete Buffers to the Left" },
-			{ "[b", "<cmd>BufferLineCyclePrev<CR>", desc = "Buffer: Previous Buffer" },
-			{ "]b", "<cmd>BufferLineCycleNext<CR>", desc = "Buffer: Next Buffer" },
-			{ "[B", "<cmd>BufferLineMovePrev<CR>", desc = "Buffer: Move Buffer Previous" },
-			{ "]B", "<cmd>BufferLineMoveNext<CR>", desc = "Buffer: Move Buffer Next" },
-		},
-  }
+    keys = {
+      { leader .. "p", "<cmd>BufferLineTogglePin<CR>", desc = "Buffer: Toggle Pin" },
+      { leader .. "P", "<cmd>BufferLineGroupClose ungrouped<CR>", desc = "Buffer: Delete Non-Pinned Buffers" },
+      { leader .. "r", "<cmd>BufferLineCloseRight<CR>", desc = "Buffer: Delete Buffers to the Right" },
+      { leader .. "l", "<cmd>BufferLineCloseLeft<CR>", desc = "Buffer: Delete Buffers to the Left" },
+      { leader .. "p", "<cmd>BufferLineCyclePrev<CR>", desc = "Buffer: Previous Buffer" },
+      { leader .. "n", "<cmd>BufferLineCycleNext<CR>", desc = "Buffer: Next Buffer" },
+      { leader .. "P", "<cmd>BufferLineMovePrev<CR>", desc = "Buffer: Move Buffer Previous" },
+      { leader .. "N", "<cmd>BufferLineMoveNext<CR>", desc = "Buffer: Move Buffer Next" },
+      { leader .. "o", "<cmd>BufferLineCloseOthers<CR>", desc = "Buffer: Close Other Buffers" },
+      { leader .. "D", "<cmd>BufferLinePickClose<CR>", desc = "Buffer: Close Buffer (Pick)" },
+      {
+        leader .. "s",
+        function()
+          vim.cmd "split"
+          vim.cmd "BufferLinePick"
+        end,
+        desc = "Buffer: Split in Horizontal Split",
+      },
+      {
+        leader .. "S",
+        function()
+          vim.cmd "vsplit"
+          vim.cmd "BufferLinePick"
+        end,
+        desc = "Buffer: Split in Vertical Split",
+      },
+    },
+  },
 }
