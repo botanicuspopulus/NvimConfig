fa2edafc7e5c18c4ac62255e0018f9740cec7ffd
William Smith <wsmith1@csir.co.za>
2025-12-08 13:03:26 +0200

    feat: adding file type grouping for bufferline

--- dot-config/nvim/lua/plugins/bufferline.lua
+++ dot-config/nvim/lua/plugins/bufferline.lua
@@ -1,3 +1,76 @@
+local function split_name(buf)
+  local name = vim.fn.fnamemodify(buf.path, ":t")
+  local ext = vim.fn.fnamemodify(buf.path, ":e")
+
+  return name, ext
+end
+
+local groups = {
+  options = {
+    toggle_hidden_on_enter = true,
+  },
+  items = {
+    {
+      name = "C / C++ / CMake",
+      highlight = { underline = true, sp = "cyan" },
+      matcher = function(buf)
+        local name, ext = split_name(buf)
+        local match_options = { "c", "cpp", "h", "hpp", "cc", "cxx", "hh", "hxx", "cmake" }
+        return vim.tbl_contains(match_options, ext) or name == "CMakeLists.txt"
+      end,
+    },
+    {
+      name = "Python",
+      highlight = { underline = true, sp = "cyan" },
+      matcher = function(buf)
+        local name, ext = split_name(buf)
+        return ext == "py" or name == "pyproject.toml"
+      end,
+    },
+    {
+      name = "Lua",
+      highlight = { underline = true, sp = "red" },
+      matcher = function(buf)
+        return vim.fn.fnamemodify(buf.path, ":e") == "lua"
+      end,
+    },
+    {
+      name = "JSON",
+      highlight = { underline = true, sp = "yellow" },
+      matcher = function(buf)
+        return vim.fn.fnamemodify(buf.path, ":e") == "json"
+      end
+    },
+    {
+      name = "Docs",
+      highlight = { underline = true, sp = "magenta" },
+      matcher = function(buf)
+        local match_options = { "md", "markdown", "txt", "rst" }
+        local name, ext = split_name(buf)
+        return vim.tbl_contains(match_options, ext) or name == "README"
+      end,
+    },
+    {
+      name = "Shell",
+      highlight = { underline = true, sp = "orange" },
+      matcher = function(buf)
+        local name, ext = split_name(buf)
+        local match_options = { "sh", "bash", "zsh" }
+        return vim.tbl_contains(match_options, ext) or name:match("^.?%rc")
+      end,
+    },
+    {
+      name = "Containers",
+      hihlight = { underline = true, sp = "red" },
+      matcher = function(buf)
+        local name = vim.fn.fnamemodify(buf.path, ":t")
+        local options = { "Dockerfile", "dockerfile", "Containerfile" }
+        return vim.tbl_contains(options, name)
+      end,
+    }
+  },
+}
+
 return {
   {
     "akinsho/bufferline.nvim",
@@ -16,6 +89,7 @@ return {
             seperator = true,
           },
         },
+        groups = groups,
       },
     },
     config = function(_, opts)
