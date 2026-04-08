31d06dc8b78f9f2d8ea3247c933190b404d47a4c
William Smith <wsmith1@csir.co.za>
2026-04-08 12:23:57 +0200

    refactor: updating clangd handling

--- dot-config/nvim/lsp/clangd.lua
+++ dot-config/nvim/lsp/clangd.lua
@@ -11,35 +11,24 @@ local root_markers = {
   ".git",
 }
 
-local function get_project_root()
-  local root = vim.fs.root(0, root_markers)
-
-  if root then
-    return root
-  end
-
-  return vim.fn.getcwd()
-end
-
-return  {
+return {
   root_markers = root_markers,
   capabilities = {
     offsetEncoding = { "utf-16" },
   },
   filetypes = { "c", "cpp", "cuda" },
   cmd = {
-      "clangd",
-      "--background-index",
-      "--clang-tidy",
-      "--header-insertion=iwyu",
-      "--completion-style=detailed",
-      "--function-arg-placeholders",
-      "--fallback-style=llvm",
+    "clangd",
+    "--background-index",
+    "--clang-tidy",
+    "--header-insertion=iwyu",
+    "--completion-style=detailed",
+    "--function-arg-placeholders",
+    "--fallback-style=llvm",
   },
   init_options = {
     usePlaceHolders = true,
     completeUnimported = true,
     clangFileStatus = true,
   },
-  on_attach = function(client, bufnr) client.server_capabilities.semanticTokensProvider = nil end,
 }
